8eabd0b2732c356af3eec488c4cec0b99a3eed2f
Luke Arentz <luke.arentz@dt.com.au>
2016-01-25 09:15:35 +1100

    Updated assembly information

--- Svenkle.XMLTransformer.Tests/Properties/AssemblyInfo.cs
+++ Svenkle.XMLTransformer.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Svenkle.XMLTransformer.Tests")]
+[assembly: AssemblyTitle("XML Transformer Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Svenkle.XMLTransformer.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("XMLTransformer")]
+[assembly: AssemblyCopyright("Copyright © Svenkle 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
